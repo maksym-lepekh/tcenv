ff28ca174412b40728606bd048a93b5b4e4c0539
Maksym Lepekh <mlepekh-ua@singlestore.com>
2023-12-05 10:06:16 +0200

    Add argparse library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "recipe_builder.hpp"
 #include "recipe_repo.hpp"
 
+#include <argparse/argparse.hpp>
 #include <cstdlib>
 #include <spdlog/fmt/ranges.h>
 #include <spdlog/spdlog.h>
@@ -8,27 +9,19 @@
 
 using namespace std::literals;
 
-auto main(int argc, char* argv[]) -> int
+struct test_args: argparse::Args
 {
-    if (argc != 2)
-    {
-        spdlog::info("wrong args");
-        return EXIT_FAILURE;
-    }
+    std::string& pkg_name = arg("Package to build").set_default("sed");
 
-    spdlog::set_level(spdlog::level::debug);
-
-    if (argv[1] == "test"sv)
+    int run() override
     {
-        spdlog::info("Selected 'test'");
         auto repo = recipe_repo{};
         repo.init();
         spdlog::info("Repo initialized");
 
-        constexpr auto pkg = "sed";
-        if (auto rec = repo.find_by_name(pkg))
+        if (auto rec = repo.find_by_name(pkg_name))
         {
-            spdlog::info("Found recipe for {}", pkg);
+            spdlog::info("Found recipe for {}", rec->package_name);
             builder::print_recipe(*rec);
 
             auto b_env = builder::get_env_for_pkg(rec.value());
@@ -38,7 +31,24 @@ auto main(int argc, char* argv[]) -> int
                 return EXIT_FAILURE;
             }
         }
+        return EXIT_SUCCESS;
+    }
+};
+
+struct app_args: argparse::Args
+{
+    test_args& cmd = subcommand("test");
+    bool& verbose  = flag("v,verbose", "Enable more verbose output");
+};
+
+auto main(int argc, char* argv[]) -> int
+{
+    auto args = argparse::parse<app_args>(argc, argv);
+
+    if (args.verbose)
+    {
+        spdlog::set_level(spdlog::level::debug);
     }
 
-    return EXIT_SUCCESS;
+    return args.run_subcommands();
 }
